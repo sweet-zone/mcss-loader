45f5f08b344dd06745dc3968ae99a9b07a2e3608
maggiehe <mechi.he@gmail.com>
2016-08-24 17:08:34 +0800

    support mcss options

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
-var mcss = require('mcss')
+var mcss = require('mcss'),
+  loaderUtils = require('loader-utils')
 
 module.exports = function(content) {
 	this.cacheable && this.cacheable();
 
-	var callback = this.async();
+	var callback = this.async(),
+    options = loaderUtils.parseQuery(this.query)
 	
-	mcss().translate(content).done(function(text) {
+	mcss(options).translate(content).done(function(text) {
 		callback(null, text);
 	}).fail(function(err) {
 		callback(err);
